76e2671517c0b75c9b38f5bf6ba0e983a5a7d34c
Xiaozhi112 <312146391@qq.com>
2016-01-02 23:54:18 +0800

    test change and commit to github

--- TestGit.cpp
+++ TestGit.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
 	printf("hello github.\n");
+	printf("this is a change for second commit to github.\n");
 
 	return 0;
 }
